df0a18658af7628a7872a1b5e6803524eba33912
valle1337 <alex.paulsson@stud.sti.se>
2024-05-13 16:20:21 +0200

    user can now sign in through frontend

--- src/views/LogIn.jsx
+++ src/views/LogIn.jsx
@@ -1,12 +1,74 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function LogIn() {
+
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:8080/users/signIn", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to sign in user");
+      }
+      alert("User sign in successful!");
+    } catch (error) {
+      console.error("Error signing in user:", error);
+    }
+  };
+
   return (
     <>
-      <div>Log In Page</div>
-      <Link to={"/Register"}>Register if you do not have an account!</Link>
-    </>
+<div className="flex justify-center">
+  <div className="bg-slate-200 p-8 rounded-lg shadow-md w-96">
+    <h2 className="text-2xl font-semibold mb-6">Register</h2>
+    <form onSubmit={handleSubmit} className="space-y-4">
+      <div>
+        <label htmlFor="username" className="block text-sm font-medium text-gray-700">Username</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+        />
+      </div>
+      <div>
+        <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+        />
+      </div>
+      <button type="submit" className="bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600">Sign in!</button>
+    </form>
+  </div>
+  </div>
+</>
   );
 }
 export default LogIn;
